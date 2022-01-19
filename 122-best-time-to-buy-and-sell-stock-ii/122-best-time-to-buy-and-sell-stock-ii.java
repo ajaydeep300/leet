04ef5ad79f925e40ba989aed6233b3e19f384948
Ajaydeep singh <59273429+ajaydeep300@users.noreply.github.com>
2022-01-18 22:22:18 -0500

    Time: 2 ms (25.84%), Space: 41.4 MB (12.17%) - LeetHub

--- 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.java
+++ 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.java
@@ -1,8 +1,9 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        int[] dp = new int[prices.length + 1];
-        dp[0] = 0;
+        int[] dp = new int[prices.length + 1];// new table with size prices.length + 1
+        dp[0] = 0;// intialize to 0
         for(int i = 1; i < prices.length;i++){
+            // fill table by adding last index element and profit if positive
             dp[i] = dp[i-1] + Math.max(0,prices[i] - prices[i-1]);
         }
         return dp[prices.length - 1];
