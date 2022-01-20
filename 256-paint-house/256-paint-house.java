4bc2600ef7e2998b25dda0b83f166ff157ef3c03
Ajaydeep singh <59273429+ajaydeep300@users.noreply.github.com>
2022-01-20 17:06:50 -0500

    Time: 1 ms (72.46%), Space: 40.4 MB (19.95%) - LeetHub

--- 256-paint-house/256-paint-house.java
+++ 256-paint-house/256-paint-house.java
@@ -2,7 +2,8 @@ class Solution {
     public int minCost(int[][] costs) {
         int n = costs.length;
         if(n == 0)
-            return 0 ;
+            return 0;
+        // we add the minimum of the previous index costs[i-1][] to the current colour index so that no 2 colors are same
         for(int i = 1; i < n; i++){
             costs[i][0] += Math.min(costs[i-1][1],costs[i-1][2]);
             costs[i][1] += Math.min(costs[i-1][0],costs[i-1][2]);
