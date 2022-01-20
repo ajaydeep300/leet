574d9dcf5dc250b65cbddaaf3d70733c6475fc10
Ajaydeep singh <59273429+ajaydeep300@users.noreply.github.com>
2022-01-20 16:20:52 -0500

    Time: 9 ms (36.14%), Space: 97.9 MB (42.14%) - LeetHub

--- 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.java
+++ 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.java
@@ -1,13 +1,14 @@
 class Solution {
     public int maxProfit(int[] prices) {
         //first_buy -> first_sell -> second_buy -> second_sell
-        int b1 = Integer.MIN_VALUE, b2 = Integer.MIN_VALUE;
+        // they are interdependent on each other 
+        int b1 = Integer.MAX_VALUE, b2 = Integer.MAX_VALUE;
         int s1 = 0, s2 = 0;
         for(int i = 0; i < prices.length;i++){
-            b1 = Math.max(b1,-prices[i]);
-            s1 = Math.max(s1,b1 + prices[i]);
-            b2 = Math.max(b2,s1 -prices[i]);
-            s2 = Math.max(s2,b2 + prices[i]);
+            b1 = Math.min(b1, prices[i]);
+            s1 = Math.max(s1,prices[i] - b1);
+            b2 = Math.min(b2, prices[i] - s1);
+            s2 = Math.max(s2, prices[i] - b2);
         }
         return s2;
         
