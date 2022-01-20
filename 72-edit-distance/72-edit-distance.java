b981788bd163614c673cac87b427c616c35fbbe3
Ajaydeep singh <59273429+ajaydeep300@users.noreply.github.com>
2022-01-20 18:22:06 -0500

    Time: 4 ms (93.62%), Space: 38.9 MB (77.78%) - LeetHub

---
+++ 72-edit-distance/72-edit-distance.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int minDistance(String word1, String word2) {
+        int x = word1.length();
+        int y = word2.length();
+        int[][] dp = new int[x+1][y+1];
+        for(int i = 0; i <= x;i++)
+            dp[i][0] = i;
+        for(int i = 0; i <= y ; i++)
+            dp[0][i] = i;
+        
+        for(int i = 1; i <= x;i++){
+            for(int j = 1; j <= y;j++){
+                if(word1.charAt(i-1) == word2.charAt(j-1))
+                    dp[i][j] = dp[i-1][j-1];
+                //
+                else{
+                    dp[i][j] = 1 + Math.min(dp[i-1][j-1],Math.min(dp[i-1][j],dp[i][j-1]));
+                    // increment by one and select dp[i-1][j] for deletion
+                    // dp[i][j-1] for insertion
+                    // dp [i-1][j-1] for updating
+                }
+            }
+        }
+        return dp[x][y];
+    }
+}
