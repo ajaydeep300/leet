681dc4ba791ff456293b12b42d06d03cbae2eb3a
Ajaydeep singh <59273429+ajaydeep300@users.noreply.github.com>
2022-01-20 22:40:57 -0500

    Time: 11 ms (96.47%), Space: 38.3 MB (84.45%) - LeetHub

--- 322-coin-change/322-coin-change.java
+++ 322-coin-change/322-coin-change.java
@@ -1,15 +1,18 @@
 class Solution {
     public int coinChange(int[] coins, int amount) {
-        int num = 0;
         int[] dp = new int[amount+1];
+        // declare table
         for(int i = 1; i < dp.length;i++){
             dp[i] = dp.length;
+            //fill table with the given amount
             for(int j = 0; j < coins.length;j++){
-                if(i >= coins[j])
+                if(i >= coins[j])// if amount is greater than denomination value then
                     dp[i] = Math.min(dp[i],dp[i-coins[j]] + 1);
+                // dp[i] is the minimum of amount or (amount - denomination) + 1
             }
         }
         if(dp[amount] == dp.length)
+            // if not possible then return -1
             return -1;
         else
             return dp[amount];
