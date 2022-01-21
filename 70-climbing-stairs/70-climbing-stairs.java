e8d051af1ab7af357804af153bd88a922ce58bb1
Ajaydeep singh <59273429+ajaydeep300@users.noreply.github.com>
2022-01-20 21:03:57 -0500

    Time: 0 ms (100.00%), Space: 35.8 MB (60.12%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.java
+++ 70-climbing-stairs/70-climbing-stairs.java
@@ -1,6 +1,17 @@
 class Solution {
     public int climbStairs(int n) {
-        HashMap<Integer,Integer> mem = new HashMap<>();
+        if(n == 0) return 0;
+        if(n == 1) return 1;
+        int[] dp = new int[n+1];
+        dp[0] = 0;
+        dp[1] = 1;
+        dp[2] = 2;
+        for(int i = 3; i <= n;i++){
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        return dp[n];
+    }
+/*        HashMap<Integer,Integer> mem = new HashMap<>();
         mem.put(1,1);
         mem.put(2,2);
         return add(n,mem);
@@ -12,4 +23,5 @@ class Solution {
         return memo.get(n);
 
     }
+    */
 }
