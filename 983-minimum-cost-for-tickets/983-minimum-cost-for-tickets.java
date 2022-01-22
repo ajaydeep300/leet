e1e6b9352a8ae3a755d5f4184ba0584e0c761697
Ajaydeep singh <59273429+ajaydeep300@users.noreply.github.com>
2022-01-21 21:06:54 -0500

    Time: 2 ms (51.36%), Space: 39.3 MB (13.02%) - LeetHub

--- 983-minimum-cost-for-tickets/983-minimum-cost-for-tickets.java
+++ 983-minimum-cost-for-tickets/983-minimum-cost-for-tickets.java
@@ -1,17 +1,24 @@
 class Solution {
     public int mincostTickets(int[] days, int[] costs) {
         int max = days[days.length - 1];
+        // max represents the last day of travel or max day of travel
         int[] dp= new int[max + 1];
+        // we make a new array with size = max; which incluedes non travelling days as well
         HashSet<Integer> s = new HashSet<>();
+        // add all travelling days to the hash set
         for(int i : days)
             s.add(i);
         for(int i = 1;i < dp.length;i++){
+            // a,b,c are the values of previous indexes a = i-1, b = i-7, c = i-30
+            // if i-7, i-30 are negative then we take value 0 otherwise dp[i-7]or dp[i-30]
             int a = dp[i-1];
             int b = (i - 7) > 0 ? dp[i-7] : 0;
             int c = (i - 30) > 0 ? dp[i-30] : 0;
             if(s.contains(i))
+                // choose minimum out of the these costs
                 dp[i] = Math.min(costs[0]+a , Math.min(costs[1] + b, costs[2] + c));
             else
+                // non travelling days have the same costs of travel
                 dp[i] = dp[i-1];
         }
         return dp[dp.length - 1];
