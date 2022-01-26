c2a6a1e40d5b988b1d52cf790af84645bdcdc39a
Ajaydeep singh <59273429+ajaydeep300@users.noreply.github.com>
2022-01-26 14:33:51 -0500

    Time: 0 ms (100.00%), Space: 36.4 MB (65.95%) - LeetHub

--- 198-house-robber/198-house-robber.java
+++ 198-house-robber/198-house-robber.java
@@ -2,12 +2,16 @@ class Solution {
     public int rob(int[] nums) {
         if(nums.length == 1)
             return nums[0];
+        // if there is only one element then the return the first element
         int[] dp = new int[nums.length];
+        // declare a new dp array
         dp[0] = nums[0];
         dp[1] = Math.max(nums[0],nums[1]);
+        //initialize
         for(int i = 2;i < nums.length;i++){
                 dp[i] = Math.max(nums[i] + dp[i-2],dp[i-1]);
         }
+        // run loop and get the maximum of last dp element and the sum of 2 elements before 
         return dp[nums.length -1];
     }
 }
