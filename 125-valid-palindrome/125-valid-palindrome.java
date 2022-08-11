5678f10809693fd44ec3af8586d354de95da2274
Ajaydeep singh <59273429+ajaydeep300@users.noreply.github.com>
2022-08-11 16:56:58 -0400

    Time: 5 ms (78.22%), Space: 43.3 MB (77.89%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.java
+++ 125-valid-palindrome/125-valid-palindrome.java
@@ -1,26 +1,26 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        int i = 0;
-        int j = s.length() - 1;
-        if(s.isEmpty()) return true;
-        while(i<=j){
-        if(!Character.isLetterOrDigit(s.charAt(i))){
-            i++;
-        }
-            // check if character is a letter or digit, if not then increment index from left by 1
-        else if(!Character.isLetterOrDigit(s.charAt(j))){
-            j--;
-        }
-            // check if character is a letter or digit, if not then decrement ind3x from right by 1
-        else{
-            if(Character.toLowerCase(s.charAt(i)) != Character.toLowerCase(s.charAt(j))){
-                return false;
+        int start = 0;
+        int end = s.length() - 1;
+        if(s.isEmpty())
+            return true;
+        while(start <= end){
+            if(!Character.isLetterOrDigit(s.charAt(start))){
+                start += 1;
             }
-            i++;
-            j--;      
-        }
-        // check if characters are equal or not, if not then return false else update index to check the next characters
-    }
-             return true;
+            else if(!Character.isLetterOrDigit(s.charAt(end))){
+                end -= 1;
+            }
+            else{
+                if(Character.toLowerCase(s.charAt(start)) != Character.toLowerCase(s.charAt(end))){
+                    return false;
+                }
+                else{
+                    start += 1;
+                    end -= 1;
+                }
+            }
+        }    
+        return true;
  }
 }
