843dc86deeb42a82d99da79dd865d5e3e583d433
Ajaydeep singh <59273429+ajaydeep300@users.noreply.github.com>
2022-08-11 17:31:11 -0400

    Time: 0 ms (100.00%), Space: 40.7 MB (94.63%) - LeetHub

--- 75-sort-colors/75-sort-colors.java
+++ 75-sort-colors/75-sort-colors.java
@@ -1,16 +1,23 @@
 class Solution {
     public void sortColors(int[] nums) {
-        int i = 0,j = 0,k = 0;
-        for(int a = 0;a< nums.length;a++){
-            if(nums[a] == 0) i++;
-            else if(nums[a] == 1) j++;
-            else k++;
+        int zero = 0;
+        int one = 0;
+        int two = 0;
+        for(int i = 0; i < nums.length; i++){
+            if(nums[i] == 0)
+                zero += 1;
+            else if(nums[i] == 1)
+                one += 1;
+            else
+                two += 1;
         }
-        // we count the number of times 0,1,2 appear
-        for(int a = 0;a<nums.length;a++){
-            if(a < i) nums[a] = 0;
-            else if(a < i+j) nums[a] = 1;
-            else nums[a] = 2;
+        for(int i = 0; i < nums.length ; i++){
+            if(i < zero)
+                nums[i] = 0;
+            else if(i < (zero + one))
+                nums[i] = 1;
+            else
+                nums[i] = 2;
         }
         // if index is less than the assigned counter for minimum value then nums[index] = value
         //similarly for the other values
