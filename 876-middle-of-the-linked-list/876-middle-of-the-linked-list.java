560424510dd60cccb134d35794616bcfb39545a5
Ajaydeep singh <59273429+ajaydeep300@users.noreply.github.com>
2022-08-13 17:51:50 -0400

    Time: 0 ms (100.00%), Space: 41.8 MB (38.76%) - LeetHub

--- 876-middle-of-the-linked-list/876-middle-of-the-linked-list.java
+++ 876-middle-of-the-linked-list/876-middle-of-the-linked-list.java
@@ -10,21 +10,16 @@
  */
 class Solution {
     public ListNode middleNode(ListNode head) {
-        ListNode temp = head;
+        ListNode cur = head;
         int size = 0;
-        while(temp != null){
-            temp = temp.next;
+        while(cur != null){
+            cur = cur.next;
             size++;
         }
-        // we calculate size then decide the half value
-        int p = 0;
-        p = ((size % 2) == 1 ? (size)/2 : (size+1)/2);
-        int j = 0;
-        while(j < p){
+        int half = (size % 2 == 0) ? ((size+1)/2) : (size/2);
+        for(int i = 0; i < half; i++){
             head = head.next;
-            j++;
         }
-        // runtime O(n+n/2) = O(n)
         return head;
     }
 }
