1cf8f392a669c8f0ce0d47df9c1271943fe93bbd
Ajaydeep singh <59273429+ajaydeep300@users.noreply.github.com>
2022-08-12 21:17:37 -0400

    Time: 0 ms (100.00%), Space: 43.8 MB (85.97%) - LeetHub

--- 141-linked-list-cycle/141-linked-list-cycle.java
+++ 141-linked-list-cycle/141-linked-list-cycle.java
@@ -11,26 +11,14 @@
  */
 public class Solution {
     public boolean hasCycle(ListNode head) {
- /*       HashMap<Integer,Boolean> cycle = new HashMap<>();
-        ListNode x = head;
-        while(x != null){
-            int y = x.val;
-            if(cycle.get(y) == null)
-                continue;
-            else if(cycle.get(y) == true)
-                return true;
-            cycle.put(y, true);
-            x = x.next;
-        }
-        return false; */
-        ListNode one = head;
-        ListNode two = head;
-        while(two != null && two.next != null){
-            one = one.next; // retrives next node one by one
-            two = two.next.next; // retrives next node by twice
-            if(one == two) // hence they will eventually match if there is a match
+        ListNode slow = head;
+        ListNode fast = head;
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+            if(slow == fast)
                 return true;
         }
-        return false; // if no match then return false
+        return false;
     }
 }
