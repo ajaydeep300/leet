62bf3172aa74dd3bd53d88730877bbae50ef772f
Ajaydeep singh <59273429+ajaydeep300@users.noreply.github.com>
2022-08-29 22:22:02 -0400

    Time: 137 ms (12.21%), Space: 114.8 MB (13.61%) - LeetHub

--- 904-fruit-into-baskets/904-fruit-into-baskets.java
+++ 904-fruit-into-baskets/904-fruit-into-baskets.java
@@ -7,17 +7,18 @@ class Solution {
         int start =0,end = 0,len = 0;
         // we continue adding fruits into a hash map until a third fruit comes up where we have to stop iterating and update the max length of the subarray, after that we delete the first fruit to continue oue evalutaion with the third fruit in hopes of getting a new max subarray.
         while(end < fruits.length){
-            int a = fruits[end];
-            ha.put(a, ha.getOrDefault(a,0) + 1);
+            int val = fruits[end];
+            ha.put(val,ha.getOrDefault(val,0) + 1);
             end++;
             while(ha.size() > 2){
-                int b = fruits[start];
-                ha.put(b,ha.get(b) - 1);
-                if(ha.get(b) == 0)
-                    ha.remove(b);
+                int rem = fruits[start];
+                ha.put(rem, ha.get(rem) - 1);
+                if(ha.get(rem) == 0){
+                    ha.remove(rem);
+                }
                 start++;
             }
-            len = Math.max(len,end-start);
+            len = Math.max(len, end - start);
         }
         return len;
     }
